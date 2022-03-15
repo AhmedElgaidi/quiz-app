c58c1f231fbc2fc702c05b5feb01081591da5664
Ahmed Elgaidi <49118737+AhmedElgaidi@users.noreply.github.com>
2022-03-15 09:44:51 +0200

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,47 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the result of 1 + 1 = ?",
+    answer: "two",
+    options: [
+      "one",
+      "two",
+      "three",
+      "four"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What is the result of 1 + 1 = ?",
+    answer: "two",
+    options: [
+      "one",
+      "two",
+      "three",
+      "four"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What is the result of 1 + 1 = ?",
+    answer: "two",
+    options: [
+      "one",
+      "two",
+      "three",
+      "four"
+    ]
+  },
+  {
+    numb: 4,
+    question: "What is the result of 1 + 1 = ?",
+    answer: "two",
+    options: [
+      "one",
+      "two",
+      "three",
+      "four"
+    ]
+  }
+];
